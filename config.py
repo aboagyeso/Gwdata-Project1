9d38bd068eb74a0af7dfba7b6b5cebd92001cdfd
aboagyeso <36752729+aboagyeso@users.noreply.github.com>
2018-07-12 09:28:28 -0400

    This is the cdc and tweeter overdose data and code

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "Lw68cO7aQ0TtZsJyHT8OAt4qk"
+consumer_secret = "yfPfbz2SrQeQQcQE9nLG9IRm7YCqxALsfIOmuZgGzatlUfkSgN"
+access_token = "1013065685600231424-WoVpqxTGggh0MXn7yWBaMxEQKwmpFw"
+access_token_secret = "Jnn0F5XR0qgFjFq3NTUUaD2juS2yTcGOXc43vARBTDsLI"
